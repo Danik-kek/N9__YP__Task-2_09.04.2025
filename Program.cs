47cd7dc35d021d5b60da08050e3dd5379d2fd9eb
Loli Admin <Loli Admin@DESKTOP-H4SR9KC>
2025-04-09 12:11:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace N9__YP__Task_2_09._04._2025
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string filePath = "numbers.txt";
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не найден.");
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(filePath);
+            Console.WriteLine("Содержимое файла:");
+
+            int componentCount = 0;
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+                string[] numbers = line.Split(new[] { ' ', '\t', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                componentCount += numbers.Length;
+            }
+
+            Console.WriteLine($"Количество компонентов: {componentCount}");
+        }
+
+    }
+}
